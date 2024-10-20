c2931613358c51903af17eb8242d17c9ec7de8a4
Musat Fabian <musatfabiandg@gmail.com>
2024-10-20 19:27:57 +0300

    Movement + Basic Room

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    private readonly KeyCode right = KeyCode.D;
+    private readonly KeyCode left = KeyCode.A;
+    private readonly KeyCode down = KeyCode.S;
+    private readonly KeyCode up = KeyCode.W;
+
+    private float horizontal = 0f;
+    private float vertical = 0f;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private GameObject direction;
+
+    [SerializeField]
+    private float maxSpeed = 5.5f;
+
+    private void Start() {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+        horizontal = Input.GetKey(right) ? 1f : (Input.GetKey(left) ? -1f : 0f);
+        vertical = Input.GetKey(up) ? 1f : (Input.GetKey(down) ? -1f : 0f);
+        movement.x = horizontal;
+        movement.y = vertical;
+        movement.Normalize();
+    }
+
+    private void FixedUpdate() {
+        rb.MovePosition(rb.position + movement * maxSpeed * Time.fixedDeltaTime);
+
+        if (movement != Vector2.zero) {
+            direction.SetActive(true);
+            float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
+            direction.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
+            direction.transform.position = rb.position + movement * 0.5f;
+        } else direction.SetActive(false);
+    }
+}
