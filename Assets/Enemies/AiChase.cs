c7bd871f2d85fea357b0857a7e491581d36ab347
Silvian <silvianachim101@gamil.com>
2024-11-16 15:51:41 +0200

    Animation working

--- Assets/Enemies/AiChase.cs
+++ Assets/Enemies/AiChase.cs
@@ -2,35 +2,57 @@ using UnityEngine;
 
 public class AiChase : MonoBehaviour
 {
-	public GameObject player;
-	public float speed;
-	public float distanceBetween;
+	public GameObject Player;
+	public float Speed;
+	public float DistanceBetween;
+	public float AttackRange;
 
 	private float distance;
+	private bool isAttacking;
+	private bool isChasing;
+	private Animator animator;
+
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		animator = GetComponent<Animator>();
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
-		distance = Vector2.Distance(transform.position, player.transform.position);
-		Vector2 direction = player.transform.position - transform.position;
+		distance = Vector2.Distance(transform.position, Player.transform.position);
+		Vector2 direction = Player.transform.position - transform.position;
 
 		direction.Normalize();
 
 		var angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
 
+		if (distance < AttackRange)
+		{
+			isAttacking = true;
+		}
+		else
+		{
+			isAttacking = false;
+		}
 
-		if (distance < distanceBetween)
+		if (distance < DistanceBetween)
 		{
 			transform.position =
-				Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+				Vector2.MoveTowards(transform.position, Player.transform.position, Speed * Time.deltaTime);
 
 			transform.rotation = Quaternion.Euler(Vector3.forward * angle);
+
+			isChasing = true;
 		}
+		else
+		{
+			isChasing = false;
+		}
+
+		animator.SetBool("isChasing", isChasing);
+		animator.SetBool("isAttacking", isAttacking);
 	}
 }
