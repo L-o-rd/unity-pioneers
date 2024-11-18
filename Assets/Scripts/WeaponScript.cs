d6c2cbb2c4f424f197822ec241afc2e72d28bef3
Daniel Gabriel Lazar <danielgabriellazar24@gmail.com>
2024-11-18 14:35:07 +0200

    Multiple weapon system

---
+++ Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapons/Weapon")]
+public class Weapon : ScriptableObject
+{
+    public string weaponName;         // Name of the weapon
+    public GameObject bulletPrefab;   // Prefab for the bullet
+    public float bulletForce;         // Force applied to the bullet
+    public float fireRate;            // Rate of fire (bullets per second)
+    public Sprite weaponSprite;       // Sprite for the weapon in UI or as a visual
+    public Sprite gunSprite;          // Sprite for the gun (used to change the gun's appearance)
+}
